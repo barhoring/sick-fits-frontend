23eaee0d1d65e7a4892491fa695fd2804e007244
Bar Horing Amir <bar.apple@gmail.com>
2021-02-28 16:05:21 +0200

    Hooking up our File input and Form Styles

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,14 +1,31 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Nice Shoes',
     price: 34234,
     description: 'These are the best shoes!',
   });
   return (
-    <div>
-      <form>
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(inputs);
+      }}
+    >
+      <fieldset>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="name">
           Name
           <input
@@ -31,14 +48,19 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
-        <button type="button" onClick={clearForm}>
-          {' '}
-          Clear Form
-        </button>
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button>
-      </form>
-    </div>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
