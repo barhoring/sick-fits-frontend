af675c7bfd5a2974da82e0d102d09037b936ca4b
Bar Horing Amir <bar.apple@gmail.com>
2021-03-03 09:09:45 +0200

    Deleting Products

--- components/Product.js
+++ components/Product.js
@@ -3,6 +3,7 @@ import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
 
 export default function Product({ product }) {
   return (
@@ -21,6 +22,7 @@ export default function Product({ product }) {
         <Link href={{ pathname: 'update', query: { id: product.id } }}>
           Edit ✏️
         </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
       </div>
       {/* TODO Add buttons to edit and delete items */}
     </ItemStyles>
