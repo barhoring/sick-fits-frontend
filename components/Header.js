ad87dc7562475e1769c1ad860f1f95e4efd31c6a
Bar Horing Amir <bar.apple@gmail.com>
2021-03-11 12:54:27 +0200

    Implementing Search

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
 import Cart from './Cart';
+import Search from './Search';
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -43,6 +44,7 @@ export default function Header() {
         <Nav />
       </div>
       <div className="sub-bar">
+        <Search />
         <p>Search</p>
       </div>
       <Cart />
